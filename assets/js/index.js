ca3ac838d4b0451bd9dba854ee90f42dc052fb0d
elduke717 <spacematcha@proton.me>
2023-08-30 16:54:41 -0400

    add post functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,15 +4,120 @@ console.log(date);
 // Add the current date to the page
 document.getElementById("date").innerText = date.toLocaleString();
 
+// Declare tasks variable to hold all the tasks on the server
 let tasks;
 
-// get all the tasks and return them to the client
-// fetch('http://localhost:3001/tasks').then((res) => {
-//     res.json();
-// }).then((data) => {
-//     tasks = data;
+// Get all tasks and post them to the task page
+fetch("http://localhost:3001/tasks")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("data", data);
+    tasks = data;
+    // Loop through the tasks and post them to the task page
+    tasks.forEach((task) => {
+      const taskDiv = document.createElement("div");
+      taskDiv.className = "task";
+      taskDiv.setAttribute("data-id", task._id);
 
-// }
-// ).catch((err) => {
-//     console.log(err);
-// } );
+      // Add task title
+      const taskTitle = document.createElement("h2");
+      taskTitle.textContent = task.title;
+      taskDiv.appendChild(taskTitle);
+
+      // Add task description
+      const taskDescription = document.createElement("p");
+      taskDescription.textContent = task.description;
+      taskDiv.appendChild(taskDescription);
+
+      // Add task due date
+      const taskDueDate = document.createElement("p");
+      const dueDate = new Date(task.due);
+      taskDueDate.textContent = `Due: ${dueDate.toDateString()}`;
+      taskDiv.appendChild(taskDueDate);
+
+      // Append delete button to the task div
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete Task";
+      deleteButton.className = "delete-button";
+      deleteButton.setAttribute("data-id", task._id);
+      taskDiv.appendChild(deleteButton);
+
+      // Append the new task div element to the task list
+      document.querySelector("#task-list").appendChild(taskDiv);
+    });
+  })
+  .catch((err) => console.error(err));
+
+// Rest of the code remains the same, including the event listeners for creating and deleting tasks
+// ...
+
+// Add a new task
+const taskForm = document.getElementById("task-form");
+const tasksContainer = document.getElementById("tasks");
+
+taskForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(taskForm);
+  const taskData = {
+    title: formData.get("title"),
+    description: formData.get("description"),
+    due: formData.get("due"),
+  };
+
+  try {
+    const response = await fetch("http://localhost:3001/tasks", {
+      // Post to the /tasks route
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(taskData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      // Clear form fields after successful submission
+      taskForm.reset();
+
+      // Fetch and update the task list after successful task creation
+      const tasksResponse = await fetch("/tasks"); // Fetch the updated task list
+      const tasksData = await tasksResponse.json();
+
+      // Update the tasksContainer with the new task list
+      tasksContainer.innerHTML = "";
+      tasksData.forEach((task) => {
+        const taskDiv = document.createElement("div");
+        taskDiv.className = "task";
+        // ... Create and append elements for each task (similar to your previous code)
+        tasksContainer.appendChild(taskDiv);
+      });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  location.reload();
+});
+
+// Delete a task
+document.addEventListener("click", async (event) => {
+  if (event.target.classList.contains("delete-button")) {
+    const taskId = event.target.getAttribute("data-id");
+    const taskDiv = event.target.parentElement;
+
+    try {
+      const response = await fetch(`http://localhost:3001/tasks/${taskId}`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      } else {
+        taskDiv.style.display = "none";
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+});
--- server/server.js
+++ server/server.js
@@ -4,6 +4,18 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3001;
 
+// Add live reload
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+app.use(connectLiveReload());
+
 app.use(bodyParser.json());
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -16,60 +28,32 @@ app.use(express.json());
 // invoke cookieParser
 app.use(cookieParser());
 
+const Task = require("../model/taskModel"); // Import your Task model
+
 // serve static files
 app.use("/assets", express.static(path.resolve(__dirname, "../assets")));
 
 // server index.html page when request to the root is made
 app.get("/", (req, res) => {
-  console.log("get /");
   return res.sendFile(path.join(__dirname + "/../views/index.html"));
 });
 
 // serve the tasks view
-app.get("/tasks", (req, res) => {
-  return res.sendFile(path.join(__dirname + "/../views/tasks.html"));
+app.get("/tasklist", (req, res) => {
+  return res.sendFile(path.join(__dirname + "/../views/tasklist.html"));
 });
 
-// get method on entries to see all blog entries
+// get all the tasks from the database
 app.get("/tasks", taskController.getAllTasks, (req, res) => {
-  // get all entries in the database
-  return res.status(200).json(res.locals.tasks);
+  return res.status(200).json(res.locals.tasks)
 });
 
-// // Get a task by id
-// app.get("/tasks/:id", (req, res) => {
-//   const task = tasks.find((t) => t.id === parseInt(req.params.id));
-//   if (!task) return res.status(404).send("Task not found.");
-//   res.json(task);
-// });
-
-// // Post a new task
-// app.post("/tasks", (req, res) => {
-//   const task = {
-//     id: tasks.length + 1,
-//     description: req.body.description,
-//   };
-//   tasks.push(task);
-//   res.status(201).json(task);
-// });
-
-// // Update a task by id
-// app.put("/tasks/:id", (req, res) => {
-//   const task = tasks.find((t) => t.id === parseInt(req.params.id));
-//   if (!task) return res.status(404).send("Task not found.");
-
-//   task.description = req.body.description || task.description;
-//   res.json(task);
-// });
-
-// // Delete a task by id
-// app.delete("/tasks/:id", (req, res) => {
-//   const taskIndex = tasks.findIndex((t) => t.id === parseInt(req.params.id));
-//   if (taskIndex === -1) return res.status(404).send("Task not found.");
-
-//   const removedTask = tasks.splice(taskIndex, 1);
-//   res.json(removedTask[0]);
-// });
+// post a new task
+app.post(
+  "/tasks",
+  taskController.createTask,
+  (req, res) => res.status(200).json(res.locals.task)
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`);
